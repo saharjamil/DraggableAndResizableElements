293ddf1a4522c46e03e90809f6bcab089cdfbbc4
saharjamil <s.vesaaghatijamil@gmail.com>
2025-04-26 16:15:31 +0330

    update:: route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,5 @@ import { Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 
 export const routes: Routes = [
-    { path:'',redirectTo:'home',pathMatch:'full'},
-    { path: 'home', component: AppComponent },
+    { path: '', component: AppComponent },
 ];
